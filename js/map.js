f7928ac139817809b8858b7c2c893d14f8bd5080
Jake Heggelke <jacob.heggelke@gmail.com>
2015-11-15 21:06:23 -0500

    working!

--- js/map.js
+++ js/map.js
@@ -6,28 +6,85 @@ var myNeighborhood = 'Brooklyn Heights, Brooklyn, NY';
 
 //map dependencies
 var geocoder;
-function initMap() {
+var map;
+function initMap(address, callback) {
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: {lat: 40.6960105, lng: -73.9932872},
     scrollwheel: false,
     zoom: 15
   });
-  geocoder = new google.maps.Geocoder();
-  geocodeAddress(geocoder, map);
+
+  coolPlaces.forEach(function(e, i){
+    queryGooglePlaces(e.name, placeMarkers, i);
+  })
+
+window.setTimeout(callback,500);
+
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = myNeighborhood;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+function queryGooglePlaces(placeName, functionToRun, i){
+  var service = new google.maps.places.PlacesService(map);
+  var request = {
+    query: placeName
+  };
+  service.textSearch(request, function(results, status){functionToRun(results, status, i)});
+}
+var infowindow;
+function placeMarkers(results, status, i) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    // add lat, lng to array for retrieval later
+    if (coolPlaces[i].lat == "") {
+      coolPlaces[i]['lat'] = results[0].geometry.location.lat();
+      coolPlaces[i]['lng'] = results[0].geometry.location.lng();
     }
-  });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: coolPlaces[i]['lat'], lng: coolPlaces[i]['lng']}
+      });
+      coolPlaces[i]['marker'] = marker;
+      infowindow = new google.maps.InfoWindow();
+      marker.addListener('click', function() {
+        var infoContent = coolPlaces[i].name;
+        changeInfoWindow(marker, infoContent);
+        toggleBounce(marker);
+      });
+    }
+    google.maps.event.addListener(infowindow,'closeclick',function(){
+      stopBounces();
+    });
+}
+
+
+function changeInfoWindow (marker, content) {
+  infowindow.close();
+  infowindow.setContent(content);
+  toggleBounce(marker);
+  infowindow.open(map, marker);
+}
+
+function centerMarker(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+  }
+}
+
+// Sets the map on all markers in the array.
+function displayFilteredMarkers() {
+  for (var i = 0; i < coolPlaces.length; i++) {
+    if (coolPlaces[i]['visible'] == true) {
+      coolPlaces[i]['marker'].setMap(map);
+    } else {coolPlaces[i]['marker'].setMap(null);}
+  }
+}
+
+function stopBounces() {
+  coolPlaces.forEach(function(e, i) {
+    coolPlaces[i]['marker'].setAnimation(null);
+  })
+}
+
+function toggleBounce(e) {
+    stopBounces();
+    e.setAnimation(google.maps.Animation.BOUNCE);
 }
--- js/sidebar-view.js
+++ js/sidebar-view.js
@@ -1,28 +1,64 @@
 var coolNeighborhood = 'Brooklyn Heights';
 
-var coolPlaces = [
-  {name: 'Henry Street Ale House',
-  tags: ['bar', 'restaurant', 'regular spot']},
-  description: 'Favorite bar in the neighborhood. Plenty of good beers on tap, good burgers, and delicious rotating specials. Waitress who wears sweatpants has not been there the past few times, so that is a plus.',
-  rating: '8/10'},
+var coolPlaces = [];
 
-  {name: 'Pair Wine and Cheese',
-  tags: ['wine bar', 'cheese!', 'off the radar'],
-  description: 'This place always seems empty when you walk in, but I have no idea why. You walk in and it smells like you are in a grilled cheese! I will usually come here for some sort of fancy cheese and wine dinner. You can&#8217;t pass that up!',
-  rating: '9/10'},
+var initializeData = function(){
+var Place = function(data) {
+  var self = this;
+  this.name = ko.observable(data.name);
+  this.description = ko.observable(data.description);
+  this.rating = ko.observable(data.rating);
+  this.tags = ko.observableArray(data.tags);
+  this.marker = ko.observable(data.marker);
+  this.dontmiss = ko.observable(data.dontmiss);
+}
 
-  {name: 'Brooklyn Borough Hall Saturday Greenmarket',
-  [tags: 'compost', 'fresh veggies', 'tag3'],
-  description: 'Lots of great booths at this farmer&#8217;s market. This is the best place in the neighborhood to drop off your weekly compost. Warm cider in the autumn-time. Tasty squashes in the autumn-time. Apple cider donuts... in the autumn-time. Looks like I have only been going here for a few months. Will report back about other seasonal vegatables.',
-  rating: '7/10'},
+var SidebarModel = function() {
+	var self = this;
+	self.placeList = ko.observableArray([]);
 
-  {name: 'Place 4',
-  [tags: 'tag1', 'tag2', 'tag3'],
-  description: 'This is another boring description',
-  rating: '4/10'},
+	coolPlaces.forEach(function(placeItem, i){
+		self.placeList.push(new Place(placeItem));
+	})
 
-  {name: 'Place 5',
-  [tags: 'tag1', 'tag2', 'tag3'],
-  description: 'Blah blha lkahfa',
-  rating: '3/10'},
-]
+	self.currentPlace = ko.observable(this.placeList()[0]);
+
+	self.switchPlace = function(){
+		self.currentPlace(this);
+    queryGooglePlaces(this.name(), centerMarker);
+    var infoContent = this.name();
+    changeInfoWindow(this.marker(), infoContent);
+    toggleBounce(this.marker());
+	};
+
+  self.filterParameter = ko.observable();
+
+  self.filterPlaces = function(){
+    self.placeList([]);
+    // Search through object - http://stackoverflow.com/a/5288882/3083666
+    $.each(coolPlaces, function(i, v) {
+          if (v.name.search(new RegExp(self.filterParameter())) != -1) {
+              self.placeList.push(new Place(v));
+              coolPlaces[i]['visible'] = true;
+              return;
+          } else {coolPlaces[i]['visible'] = false;}
+      });
+      displayFilteredMarkers();
+      return true;
+    }
+}
+
+ko.applyBindings(new SidebarModel())
+window.setTimeout(function(){$('#loading-overlay').toggle()}, 1500)
+}
+
+var spreadsheetUrl = "https://spreadsheets.google.com/feeds/list/1vqi68E7RdQyBpREXh4tTfWQA9P2H00bC2zzQE3vm430/od6/public/values?alt=json";
+$.getJSON(spreadsheetUrl, function(data) {
+ var entry = data.feed.entry;
+ $(entry).each(function(){
+   var tempTagArray = this.gsx$tags.$t.split(', ');
+   coolPlaces.push({'name':this.gsx$name.$t, 'tags': tempTagArray, 'description':this.gsx$description.$t, 'rating':this.gsx$rating.$t, 'dontmiss': this.gsx$dontmiss.$t, 'lat': '', 'lng': '', 'visible': true, 'marker': null});
+ });
+
+ initMap(myNeighborhood, function() {initializeData();});
+});
